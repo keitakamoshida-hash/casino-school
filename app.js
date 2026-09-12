91d097049c349359a66c2b386934ea4fb06e2b35
keitakamoshida-hash <keita.kamoshida@senjinholdings.com>
2026-09-13 00:30:46 +0900

    Refresh PWA assets on update

--- app.js
+++ app.js
@@ -77,4 +77,4 @@ document.addEventListener('change',e=>{const {id,value,checked}=e.target;if(id==
 attachSqueeze(s,render,save);
 attachChipDrag(s,render,message=>{document.querySelector('#error').textContent=message;});
 render();
-if(typeof navigator!=='undefined'&&'serviceWorker'in navigator)window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js').catch(()=>{}));
+if(typeof navigator!=='undefined'&&'serviceWorker'in navigator)window.addEventListener('load',()=>{navigator.serviceWorker.register('./sw.js').then(registration=>registration.update()).catch(()=>{});navigator.serviceWorker.addEventListener('controllerchange',()=>window.location.reload());});
